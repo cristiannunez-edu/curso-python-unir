eefaf53dd700769077e7f666b6978506b2058ea3
Cristian Nuñez <cristiannunez@alumno.utesa.edu>
2026-06-11 15:33:17 +0000

    Definiendo los nombres de las funciones

---
+++ 1_Sintaxis_Python/calculadora_promedios.py
@@ -0,0 +1,24 @@
+materias = []
+calificaciones = []
+
+def ingresar_calificaciones():
+    materia = input("Ingrese el nombre de la materia: ")
+    calificación = input("Ingrese la calificación: ")
+
+    materias.append(materia)
+    calificaciones.append(calificación)
+
+def calcular_promedio(calificaciones):
+    return ""
+
+def determinar_estado(calificaciones, umbral):
+    return ""
+
+def encontrar_extremos(calificaciones):
+    return ""
+
+def main():
+    ingresar_calificaciones()
+
+if __name__ == "__main__":
+    main()
